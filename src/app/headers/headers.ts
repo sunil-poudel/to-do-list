ff04b013c552047d7358f574973568b9e666c350
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-05 19:43:49 +0545

    create header component

---
+++ src/app/headers/headers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headers',
+  imports: [],
+  templateUrl: './headers.html',
+  styleUrl: './headers.css'
+})
+export class Headers {
+
+}
