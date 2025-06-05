fe3b82fa8b5658b4f93dd68efbbde297f860909e
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-05 22:49:04 +0545

    make page 50% on big screen and 100% on small screen (width of elements)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {Tasks} from './tasks/tasks';
+import {Task} from './task/task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Tasks],
+  imports: [RouterOutlet, Header, Tasks, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
