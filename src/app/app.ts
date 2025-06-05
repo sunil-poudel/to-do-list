ec014b8c1161067ff6d4d42be0248761e507f0f6
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-05 22:30:02 +0545

    maintain basic framework of tasks component, i.e. text aligns and borders

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
+import {Tasks} from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
