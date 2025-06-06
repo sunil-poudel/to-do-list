9483abf63da46fb2081cdc6974dd9b660988f054
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-06 08:54:59 +0545

    add 6 div containers and maintain position and width on menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {Tasks} from './tasks/tasks';
 import {Task} from './task/task';
+import {Menu} from './menu/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Tasks, Task],
+  imports: [RouterOutlet, Header, Tasks, Task, Menu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
