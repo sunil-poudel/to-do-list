d189f4631d3202e8f9b7452a8e5df20ac487e44f
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-19 23:13:06 +0545

    duplicate app to dummy-app:

---
+++ src/dummy-app/menu/menu.ts
@@ -0,0 +1,74 @@
+import {Component, EventEmitter, Output, inject} from '@angular/core';
+import {NewTask} from './new-task/new-task';
+import {TaskData} from '../shared/shared'
+import {TasksServices} from '../shared/tasks.services';
+
+@Component({
+  selector: 'app-menu',
+  imports: [
+    NewTask
+  ],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css'
+})
+export class Menu {
+  displayFlag: boolean = false;
+  editDisplayFlag: boolean = false;
+  submittedTask!: TaskData;
+  protected tasksServices = inject(TasksServices);
+  currentIndex?: number;
+  currentTask?: {id: number; title: string; date: string; description:string};
+
+  isSave: boolean = false;
+  isEdit: boolean = true;
+
+  onClickAdd(){
+    this.displayFlag = true;
+    this.isSave = true;
+    this.isEdit = false;
+  }
+
+  onClickEdit(){
+    if(this.getCurrentTask()) {
+      this.editDisplayFlag = true;
+      this.isEdit = true;
+      this.isSave = false;
+      // console.log(this.editDisplayFlag);
+    } else{
+      this.isEdit = false;
+      this.isSave = false;
+    }
+  }
+
+  getCurrentIndex(){
+    this.currentIndex = this.tasksServices.getCurrentTask();
+  }
+  getCurrentTask(){
+    this.getCurrentIndex();
+    if(this.currentIndex != undefined) {
+      this.currentTask = this.tasksServices.getTaskById(this.currentIndex);
+    }
+    return this.currentTask;
+  }
+
+
+  getDisplayFlag(displayFlag: boolean){
+    this.displayFlag = displayFlag;
+  }
+  getEditDisplayFlag(editDisplayFlag: boolean){
+    this.editDisplayFlag = editDisplayFlag;
+  }
+
+  getSubmittedTask(submittedTask: TaskData){
+    this.submittedTask = submittedTask;
+    // console.log(submittedTask);
+    this.sendSubmittedTaskToApp();
+  }
+
+
+  @Output() submittedTaskToApp  = new EventEmitter<{ taskData: TaskData, isSave:boolean, isEdit: boolean }>();
+  sendSubmittedTaskToApp(){
+    this.submittedTaskToApp.emit({taskData:this.submittedTask, isSave:this.isSave, isEdit: this.isEdit});
+  }
+
+}
