84048be0fdbe43fcb41955041e71fc2b44b84deb
Sunil Poudel <suneelpdl90@gmail.com>
2025-06-19 22:46:52 +0545

    flip dummy and original task services

--- src/app/shared/dummy.tasks.services.ts
+++ src/app/shared/dummy.tasks.services.ts
@@ -3,10 +3,8 @@ import {inject, Injectable, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn: "root"})
-export class TasksServices{
+export class DummyTasksServices{
   private tasks: {id: number, title:string, date:string, description:string}[] = [];
-  private archivedTasks: {id:number, title:string, date:string, description:string}[] = [];
-  private taskFromDatabase: {id:number, title:string, date:string, description:string}[] = [];
 
   private httpClient = inject(HttpClient);
 
@@ -15,79 +13,10 @@ export class TasksServices{
     this.getTaskFromBackend();
   }
 
-
-  isArchived:boolean = false;
-
-  setArchived(isArchived:boolean){
-    this.isArchived = isArchived;
-  }
-
-  flipArchived(){
-    this.isArchived = !this.isArchived;
-  }
-
-  getArchivedStatus(){
-    return this.isArchived;
-  }
-
   addTask(taskData: TaskData){
     // this.tasks.unshift(taskData);
     this.postTaskToBackend(taskData);
   }
-
-  getTasks(){
-    if(this.isArchived){
-      return this.archivedTasks;
-    } else {
-      return this.tasks;
-    }
-  }
-
-  // getTaskById(id:number){
-  //   return this.getTasks().find((task)=>task.id === id);
-  // }
-  currentTask?:{id:number, title:string, date:string, description:string};
-
-  // setCurrentTask(id:number){
-  //   this.currentTask = this.getTasks().find((task)=>task.id === id);
-  //   // console.log("current task set as "+this.currentTask?.title);
-  // }
-
-
-  getCurrentTask(){
-    return this.currentTask?.id;
-  }
-
-
-  // deleteTask(id: number){
-  //   this.tasks = this.tasks.filter((task)=> task.id != id);
-  // }
-
-  // editTask(id: number, taskData: TaskData){
-  //   const task = this.getTaskById(id);
-  //   if(task) {
-  //     task.title = taskData.title;
-  //     task.date = taskData.date;
-  //     task.description = taskData.description;
-  //   }
-  // }
-
-  // archiveTask(id: number){
-  //   const taskTemp = this.tasks.find((task)=> task.id === id);
-  //   if(taskTemp) {
-  //     this.archivedTasks.unshift(taskTemp);
-  //     this.deleteTask(id);
-  //   }
-  // }
-
-  // markTaskIncomplete(id: number){
-  //   const taskTemp = this.archivedTasks.find((task)=> task.id === id);
-  //   if(taskTemp){
-  //     this.tasks.push(taskTemp);
-  //     this.tasks = this.tasks.sort((a,b)=>b.id-a.id);
-  //     this.archivedTasks = this.archivedTasks.filter((task)=>task.id != id);
-  //   }
-  // }
 
   getTaskFromBackend(){
     //to fetch data from backend right at the start when this component becomes available
@@ -96,7 +25,7 @@ export class TasksServices{
     >("http://localhost:8080/apis/tasks").subscribe(
       {
         next: (task)=>{
-          this.taskFromDatabase = task;
+          this.tasks = task;
           console.log(task);
         }
       }
